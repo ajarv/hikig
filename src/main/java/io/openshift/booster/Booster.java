a4c168337f0b9734fb54af3fcd01344aa96ed49c
ajarv <ajarv@hotmail.com>
2018-07-29 10:57:47 -0700

    Update Booster.java

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,26 @@
-package io.openshift.booster;                                                      
-public class Booster {                                                      
- public static void main(String[] args) { }                                                      
-} 
+package io.openshift.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Booster {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Booster.class, args);
+
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        /*
+         *  @TODO   FIT Training Task 1
+        Modify greeting message
+         */
+        return "Hello World in San Diego";
+    }
+
+}
